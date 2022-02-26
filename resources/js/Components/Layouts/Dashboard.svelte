88398b6fb12c65c90085695856c1b5d8b0d1dcc5
Dan Pegg <daniel.pegg7849@gmail.com>
2022-02-26 19:59:01 +0000

    Fix dropdown

--- resources/js/Components/Layouts/Dashboard.svelte
+++ resources/js/Components/Layouts/Dashboard.svelte
@@ -99,7 +99,7 @@
                 </button>
               </div>
 
-              <div class="{dropdownMenu ? "transform opacity-100 scale-100" : "transform opacity-0 scale-95"} origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+              <div class="{dropdownMenu ? "" : "hidden"} origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                 <!-- Active: "bg-gray-100", Not Active: "" -->
                 <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
   
