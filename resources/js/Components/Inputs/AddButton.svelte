aebc47cf6e2bb518dd57a2cfb243383171e86df6
Dan Pegg <daniel.pegg7849@gmail.com>
2022-02-28 23:40:46 +0000

    Shuffle layout, add button

---
+++ resources/js/Components/Inputs/AddButton.svelte
@@ -0,0 +1,5 @@
+<button type="button" class="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+    <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd" />
+    </svg>
+</button>
--- resources/js/Pages/Dashboard/Apps/Show.svelte
+++ resources/js/Pages/Dashboard/Apps/Show.svelte
@@ -2,6 +2,7 @@
     import { Inertia } from '@inertiajs/inertia'
 
     import Dashboard from "../../../Components/Layouts/Dashboard.svelte"
+    import AddButton from "../../../Components/Inputs/AddButton.svelte"
     import TextWithToggle from "../../../Components/Inputs/TextWithToggle.svelte"
     import Toggle from "../../../Components/Inputs/Toggle.svelte"
     import WebhookCard from "../../../Components/WebhookCard.svelte"
@@ -33,26 +34,7 @@
 
     <div class="relative max-w-7xl mx-auto px-4 md:px-6 lg:px-8">
         <div class="flex space-x-4">
-            <div class="grow mx-auto rounded-lg bg-white shadow-lg border border-gray-100">
-                <div on:click={regenerateCredentials} class="cursor-pointer transition duration-200 text-center hover:bg-gray-50 active:bg-gray-100 border-b-2 border-gray-100 py-2">
-                    Regenerate Keys
-                </div>
-                <dl>
-                <div class="flex flex-col border-b border-gray-100 p-6 text-center">
-                    <dd class="order-1 text-2xl font-extrabold text-indigo-600">App ID</dd>
-                    <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">{app.appId}</dt>
-                </div>
-                <div class="flex flex-col border-t border-b border-gray-100 p-6 text-center">
-                    <dd class="order-1 text-2xl font-extrabold text-indigo-600">App Key</dd>
-                    <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">{app.appKey}</dt>
-                </div>
-                <div class="flex flex-col border-t border-gray-100 p-6 text-center">
-                    <dd class="order-1 text-2xl font-extrabold text-indigo-600">App Secret</dd>
-                    <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">{app.appSecret}</dt>
-                </div>
-                </dl>
-            </div>
-            <div class="w-2/3 grid grid-cols-2 gap-4">
+            <div class="w-2/3 grid grid-cols-1 lg:grid-cols-2 gap-x-4 lg:gap-y-16">
                 <div>
                     <p class="text-lg font-semibold">App Enabled</p>
                     <div class="rounded-lg border border-gray-100">
@@ -73,7 +55,26 @@
                 <TextWithToggle label="Max Backend Events / Second" defaultValue="0" placeholder="∞" bind:input={app.maxBackendEventsPerSecond} />
                 <TextWithToggle label="Max Client Events / Second" defaultValue="0" placeholder="∞" bind:input={app.maxClientEventsPerSecond} />
                 <TextWithToggle label="Max Read Requests / Second" defaultValue="0" placeholder="∞" bind:input={app.maxReadRequestsPerSecond} />
-            </div>    
+            </div>
+            <div class="grow mx-auto rounded-lg bg-white shadow-lg border border-gray-100">
+                <div on:click={regenerateCredentials} class="cursor-pointer transition duration-200 text-center hover:bg-gray-50 active:bg-gray-100 border-b-2 border-gray-100 py-2">
+                    Regenerate Keys
+                </div>
+                <dl>
+                <div class="flex flex-col border-b border-gray-100 p-6 text-center">
+                    <dd class="order-1 text-2xl font-extrabold text-indigo-600">App ID</dd>
+                    <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">{app.appId}</dt>
+                </div>
+                <div class="flex flex-col border-t border-b border-gray-100 p-6 text-center">
+                    <dd class="order-1 text-2xl font-extrabold text-indigo-600">App Key</dd>
+                    <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">{app.appKey}</dt>
+                </div>
+                <div class="flex flex-col border-t border-gray-100 p-6 text-center">
+                    <dd class="order-1 text-2xl font-extrabold text-indigo-600">App Secret</dd>
+                    <dt class="order-2 mt-2 text-lg leading-6 font-medium text-gray-500">{app.appSecret}</dt>
+                </div>
+                </dl>
+            </div>
         </div>
 
         <div class="mt-6 flex">
@@ -82,7 +83,10 @@
             </div>
             </div>
 
-        <div class="pt-8 pb-2">
+        <div class="flex items-center pt-8 pb-2">
+            <div class="pr-1">
+                <AddButton />
+            </div>
             <h2 class="text-lg font-semibold text-gray-900">Webhooks</h2>
         </div>
 
