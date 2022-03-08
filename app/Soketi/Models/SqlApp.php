a183ac7436c24c70354e846bdb4befbbb5df0a0f
Dan Pegg <daniel.pegg7849@gmail.com>
2022-03-08 22:46:55 +0000

    Cast JSON columns to arrays

--- app/Soketi/Models/SqlApp.php
+++ app/Soketi/Models/SqlApp.php
@@ -10,5 +10,16 @@ class SqlApp extends Model
     use HasFactory;
 
     protected $table = 'apps';
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'webhooks' => 'json',
+        'soketi_ui_settings' => 'json',
+    ];
+
     public $timestamps = false;
 }
