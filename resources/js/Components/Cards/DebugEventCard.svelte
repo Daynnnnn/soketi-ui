da0f0681df77504c24edd2ecb7939ad2cbbf0722
Dan Pegg <daniel.pegg7849@gmail.com>
2022-03-16 14:23:35 +0000

    Improve active states on debug

--- resources/js/Components/Cards/DebugEventCard.svelte
+++ resources/js/Components/Cards/DebugEventCard.svelte
@@ -29,7 +29,7 @@
     const friendlyEvent = (event, key) => events[event][key];
 </script>
 
-<div on:click={() => selectedEventMessage = i} class="cursor-pointer w-full relative rounded-lg {selectedEventMessage === i ? "border-2 border-indigo-300" : "border border-gray-300"} py-5 shadow-sm flex items-center space-x-3 transition duration-200 hover:bg-gray-50 hover:border-gray-400 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+<div on:click={() => selectedEventMessage = i} class="cursor-pointer w-full relative rounded-lg {selectedEventMessage === i ? "border-2 border-gray-600 bg-gray-50" : "border border-gray-300 hover:border-gray-400 hover:bg-gray-50"} py-5 shadow-sm flex items-center space-x-3 transition duration-200   focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
     <div class="flex flex-col items-center w-full text-center divide-y">
         <div class="w-full pb-2">
             <div class="inline-block px-2 py-0.5 rounded-full {friendlyEvent(debug_event.event_type, 'class')}">
