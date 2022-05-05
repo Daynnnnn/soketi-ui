899104935701205e96f513b693a19d43a1e454a0
Dan Pegg <daniel.pegg7849@gmail.com>
2022-05-05 12:50:00 +0100

    Use asset over mix helper

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link rel="icon" type="image/png" href="/logo.png" />
-    <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-    <script src="{{ mix('/js/app.js') }}" defer></script>
+    <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
+    <script src="{{ asset('/js/app.js') }}" defer></script>
   </head>
   <body>
     @inertia
