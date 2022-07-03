df53fbb8bf79d9dd827f0d35db832d32ab838047
Dan Pegg <daniel.pegg7849@gmail.com>
2022-07-03 12:01:40 +0100

    Start edit mode

--- resources/js/Components/Cards/WebhookCard.svelte
+++ resources/js/Components/Cards/WebhookCard.svelte
@@ -2,6 +2,7 @@
     import Button from "../Inputs/Button.svelte"
     import HookIcon from 'svelte-icons/gi/GiMeatHook.svelte'
     import LambdaIcon from '../../Icons/Lambda.svelte'
+    import Select from 'svelte-select';
 
     export let webhook;
 
@@ -18,22 +19,37 @@
         },
     };
 
-    const friendlyEventNames = {
-        client_event: 'Client Event',
-        channel_occupied: 'Channel Occupied',
-        channel_vacated: 'Channel Vacated',
-        member_added: 'Member Added',
-        member_removed: 'Member Removed',
-    };
+    const events = [
+        {value: 'client_event', label: 'Client Event'},
+        {value: 'channel_occupied', label: 'Channel Occupied'},
+        {value: 'channel_vacated', label: 'Channel Vacated'},
+        {value: 'member_added', label: 'Member Added'},
+        {value: 'member_removed', label: 'Member Removed'},
+    ];
 
+    let editMode = true;
     let type = (typeof webhook.lambda_function != 'undefined') ? 'lambda' : 'url';
+
+    function handleEventsSelect(event) {
+        webhook.event_types = [];
+        if (event.detail != null) event.detail.forEach(event => webhook.event_types.push(event.value));
+    }
+
+    function getEvents() {
+        if (webhook.event_types == null) return;
+        return events.filter(event => webhook.event_types.includes(event.value));
+    }
 </script>
 
 <div class="w-full relative rounded-lg border border-gray-300 bg-white px-6 py-3 shadow-sm items-center space-y-2 transition duration-200 hover:border-gray-400 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
     <dl class="grid grid-cols-4 gap-x-4 gap-y-4">
     <div class="sm:col-span-2">
         <dt class="text-md font-medium text-gray-500">{fieldMap[type]['title']}</dt>
+        {#if editMode}
+        <input bind:value={webhook[fieldMap[type]['key']]} class="w-full mt-1 px-2 py-1 text-md text-gray-900 border border-gray-300 rounded-sm" />
+        {:else}
         <dd class="mt-1 text-md text-gray-900">{webhook[fieldMap[type]['key']]}</dd>
+        {/if}
     </div>
     <div class="sm:col-span-2">
         <dt class="font-medium text-gray-500">Filters</dt>
@@ -49,9 +65,18 @@
     <div class="sm:col-span-2">
         <dt class="font-medium text-gray-500">Events</dt>
         <dd class="mt-1 text-gray-900">
+        {#if editMode}
+        <Select on:select={handleEventsSelect} isMulti={true} items={events} value={getEvents()}></Select>
+        {:else}
+        <!-- TODO: This is horrible -->
         {#each webhook['event_types'] ?? [] as event}
-            <p>- {friendlyEventNames[event]}</p>
+            {#each events as friendlyEvent}
+            {#if friendlyEvent['value'] === event}
+            <p>- {friendlyEvent['label']}</p>
+            {/if}
+            {/each}
         {/each}
+        {/if}
         </dd>
     </div>
     <div class="sm:col-span-2">
--- resources/js/Pages/Dashboard/Apps/Webhooks/Index.svelte
+++ resources/js/Pages/Dashboard/Apps/Webhooks/Index.svelte
@@ -1,10 +1,13 @@
 <script>
     import Dashboard from "../../../../Components/Layouts/Dashboard.svelte"
-    import Button from "../../../../Components/Inputs/Button.svelte"
     import WebhookCard from "../../../../Components/Cards/WebhookCard.svelte"
     import FaPlus from 'svelte-icons/fa/FaPlus.svelte'
 
     export let app;
+
+    const newWebhook = () => {
+        app.webhooks.push({})
+    }
 </script>
 
 <svelte:head>
@@ -17,13 +20,13 @@
             {#each app.webhooks as webhook}
                 <WebhookCard appId={app.id} {webhook} />
             {/each}
-            <a href="/apps/{app.appId}/webhooks/">
+            <button on:click={newWebhook}>
                 <div class="w-full relative rounded-lg border border-gray-300 bg-white px-6 py-3 shadow-sm items-center space-y-2 transition duration-200 hover:border-gray-400 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                    <div class="w-full inline-block align-middle text-indigo-400 h-8">
+                    <div class="w-full inline-block align-middle text-gray-400 h-8">
                         <FaPlus />                        
                     </div>
                 </div>
-            </a>
+            </button>
         </div>
     </div>
 </Dashboard>
