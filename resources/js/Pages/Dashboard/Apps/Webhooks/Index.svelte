60b0ace1d4145b1b6ccf365090f5592fecb71b3d
Dan Pegg <daniel.pegg7849@gmail.com>
2022-07-15 16:21:41 +0200

    Allow adding different types of webhooks

--- resources/js/Pages/Dashboard/Apps/Webhooks/Index.svelte
+++ resources/js/Pages/Dashboard/Apps/Webhooks/Index.svelte
@@ -6,7 +6,15 @@
 
     export let app;
 
-    const newWebhook = () => {
+    const newHttpWebhook = () => {
+        app.webhooks = [...app.webhooks, {
+            url: '',
+            filters: {},
+            headers: {},
+        }];
+    }
+
+    const newLambdaWebhook = () => {
         app.webhooks = [...app.webhooks, {
             lambda_function: '',
             filters: {},
@@ -37,13 +45,24 @@
             {#each app.webhooks as webhook, index}
                 <WebhookCard on:message={handleSave} appId={app.id} {webhook} {index} />
             {/each}
-            <button on:click={newWebhook}>
-                <div class="w-full relative rounded-lg border border-gray-300 bg-white px-6 py-3 shadow-sm items-center space-y-2 transition duration-200 hover:border-gray-400 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                    <div class="w-full inline-block align-middle text-gray-400 h-8">
-                        <FaPlus />                        
+            <div class="w-full grid grid-cols-2 gap-4">
+                <button on:click={newHttpWebhook}>
+                    <div class="relative rounded-lg border border-gray-300 bg-white px-6 py-3 shadow-sm items-center space-y-2 transition duration-200 hover:border-gray-400 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                        <div class="w-full inline-block align-middle text-gray-400 h-8">
+                            <FaPlus />                        
+                        </div>
+                        <p>Add HTTP Webhook...</p>
                     </div>
-                </div>
-            </button>
+                </button>
+                <button on:click={newLambdaWebhook}>
+                    <div class="relative rounded-lg border border-gray-300 bg-white px-6 py-3 shadow-sm items-center space-y-2 transition duration-200 hover:border-gray-400 hover:bg-gray-50 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                        <div class="w-full inline-block align-middle text-gray-400 h-8">
+                            <FaPlus />                        
+                        </div>
+                        <p>Add Lambda Webhook...</p>
+                    </div>
+                </button>
+            </div>
         </div>
     </div>
 </Dashboard>
