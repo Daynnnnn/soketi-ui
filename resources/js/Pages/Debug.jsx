bdccafbbdff033dd9414d70afd4bc9438f02f582
Dan Pegg <daniel.pegg7849@gmail.com>
2023-12-30 12:05:06 +0000

    Add height

--- resources/js/Pages/Debug.jsx
+++ resources/js/Pages/Debug.jsx
@@ -91,7 +91,7 @@ export default function Debug(props) {
 
             <div className="py-6">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className='w-full overflow-y-scroll'>
+                    <div className='w-full min-h-96 overflow-y-scroll'>
                         <div className="px-4 sm:px-6 lg:px-8">
                         <div className="mt-8 flow-root">
                             <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
