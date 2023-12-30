6b952fb41b79c0a85fa1191026e120b8188ac3a8
Dan Pegg <daniel.pegg7849@gmail.com>
2023-12-30 12:34:15 +0000

    Change primary button to indigo

--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -4,7 +4,7 @@ export default function PrimaryButton({ type = 'submit', className = '', process
             type={type}
             onClick={onClick}
             className={
-                `inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ${
+                `inline-flex items-center px-4 py-2 bg-indigo-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ${
                     processing && 'opacity-25'
                 } ` + className
             }
