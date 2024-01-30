19b79e563dde694ebd70b9a70a3bedbfa46b230d
Dan Pegg <daniel.pegg7849@gmail.com>
2024-01-30 13:29:58 +0000

    Add flag to disable signature validation

--- app/SignatureValidator/SoketiSignatureValidator.php
+++ app/SignatureValidator/SoketiSignatureValidator.php
@@ -11,6 +11,10 @@ class SoketiSignatureValidator implements SignatureValidator
 {
     public function isValid(Request $request, WebhookConfig $config): bool
     {
+        if (env('DISABLE_SIGNATURE_VALIDATION', false)) {
+            return true;
+        }
+
         $appId = $request->header('x-app-id');
 
         if ($appId == null) {
